ba4172a09e935d5d3e2e39c800bb010974bf116e
Lukas <39262485+lukaskornis@users.noreply.github.com>
2024-04-18 19:09:42 +0300

    Tiles

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,8 @@ public class Player : MonoBehaviour
 	 public float moveSpeed = 20f;
     Rigidbody2D rb;
     Vector2 input;
-
+    public GameObject landParticles;
+    bool hasLanded;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -14,13 +15,33 @@ public class Player : MonoBehaviour
     void Update()
     {
          var newInput = new Vector2( Input.GetAxisRaw( "Horizontal" ), Input.GetAxisRaw( "Vertical" ));
+         // diagonal movement fix
+         if (Mathf.Abs(newInput.x) > 0 && Mathf.Abs(newInput.y) > 0)
+         {
+	         newInput.y = 0;
+         }
+
+         if (rb.velocity.magnitude < 0.1f && !hasLanded && newInput != input && newInput != Vector2.zero)
+         {
+				 Instantiate(landParticles, transform.position, Quaternion.identity);
+				 hasLanded = true;
+         }
+
          if (newInput != Vector2.zero && rb.velocity.magnitude < 0.1f)
          {
 				 input = newInput;
 				 transform.up = -input;
+				 hasLanded = false;
          }
 
          rb.velocity = input * moveSpeed;
     }
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+	    if (other.gameObject.CompareTag("Coin"))
+	    {
+		    Destroy(other.gameObject);
+	    }
+    }
 }
